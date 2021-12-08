29eb9c2364f63ed6aa4b16db51e95c565bdd12b5
khojarbukhaitmetova <khojarbu@gmail.com>
2021-12-08 21:15:36 +0500

    Consecutive-Ones

---
+++ string_and_arrays/Consecutive_Ones.py
@@ -0,0 +1,18 @@
+# Source: https://binarysearch.com/problems/Consecutive-Ones
+
+class Solution:
+    def solve(self, nums):
+        idx = None
+        for i, j in enumerate(nums):
+            if j == 1:
+                if idx is None:
+                    idx = i+ 1
+
+                elif idx + 1 == i + 1:
+                    idx += 1
+                else:
+                    return False
+
+        if idx:
+            return True
+        return False
