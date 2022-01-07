c2912d90dad21c6d7d18df601234b5f30d5dbb2e
khojarbukhaitmetova <khojarbu@gmail.com>
2022-01-08 00:38:06 +0500

    Duplicate zeroes

--- string_and_arrays/Duplicate_Zeroes.py
+++ string_and_arrays/Duplicate_Zeroes.py
@@ -15,3 +15,14 @@ def duplicateZeros(self, arr: List[int]) -> None:
         if pos < len(arr):
             arr.insert(pos, 0)
             arr.pop()
+
+    # version 2 (Not my solution)
+
+        i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.pop()
+                arr.insert(i,0)
+                i += 2
+                continue
+            i += 1
